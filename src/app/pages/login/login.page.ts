85a73f17e37b915147597f53392432dafa2b1270
AVJDUC <a.valdiviaj@duocuc.cl>
2021-09-21 22:53:38 -0300

    cambios login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  Usuario: string;
+  Password: number;
+
+  usu = "juan";
+  clave = 1234;
+
+  constructor(private router: Router, public toastController: ToastController) { }
 
   ngOnInit() {
+  }
+
+  ingresar(){
+    //console.log(this.Usuario)
+    //console.log(this.Password)
+    if(this.Usuario == this.usu && this.Password == this.clave){
+      let navigationExtras: NavigationExtras = {
+        state: { textoEnviado: this.Usuario}
+      }
+      this.router.navigate(['/home'], navigationExtras);
+      
+    }
+    else{
+      this.presentToast("Usuario y/o Contraseña Incorrectos");
+    }
+  }
+
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000
+      }
+    );
+    toast.present();
   }
 
 }
